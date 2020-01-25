f56152d28ccb7f2b77dee1a8ba6fbc1eae417b30
Jane Ridge <rzans@yahoo.com>
2020-01-24 21:36:35 -0500

    turning100 added

--- character_input/turning100.py
+++ character_input/turning100.py
@@ -1,12 +1,15 @@
 import datetime;
 
+##This will grab the current date so that this code will work as the date chnages
 today = str(datetime.date.today());
 curr_year = int(today[:4]);
 
-
+##Let's ask the user for their current age
 yourage = input("How old are you?")
 yourage = int(yourage)
 
+##if the user is already 100 plus then we'll congradulate them, otherwise we'll
+##caclucate and print the age they'll turn 100 
 if yourage >= 100:
     print("Congraulations you've already made it to 100!")
 else:
